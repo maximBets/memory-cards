383d7040ca75edbdea8a55dd98c154f95c043433
Yuri Lopukhin_1 <yurilopukhinfol@mail.ru>
2023-04-19 22:04:00 +0300

    Созданы миграции и модели для базы данных

---
+++ db/migrations/20230419155631-create-card.js
@@ -0,0 +1,51 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      theme_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Themes',
+          key: 'id',
+        },
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      eng_word: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      rus_word: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      learned: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cards');
+  },
+};
---
+++ db/models/card.js
@@ -0,0 +1,46 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Card extends Model {
+    static associate({ Theme, User }) {
+      this.belongsTo(Theme, { foreignKey: 'id' });
+      this.belongsTo(User, { foreignKey: 'id' });
+    }
+  }
+  Card.init(
+    {
+      theme_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Themes',
+          key: 'id',
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      eng_word: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      rus_word: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      learned: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Card',
+    },
+  );
+  return Card;
+};
